59339395dbdceb25012b8f9ca9a2fb763781c8c3
shun15 <ikezushunichi0715@gmail.com>
2024-07-06 22:29:08 +0900

    first

---
+++ client_server.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+import os
+import logging
+import socket
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.0.0.1', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+@app.route('/open_file', methods=['POST'])
+def open_file():
+    try:
+        data = request.json
+        logging.debug(f"Received data: {data}")
+        file_path = data.get('file_path')
+        
+        if file_path and os.path.exists(file_path):
+            if os.name == 'nt':  # Windowsの場合
+                os.startfile(file_path)
+            elif os.name == 'posix':  # macOSの場合
+                subprocess.run(['open', file_path])
+            elif os.name == 'posix':  # Linuxの場合
+                subprocess.run(['xdg-open', file_path])
+            else:
+                return jsonify({"error": "Unsupported OS"}), 500
+            
+            logging.debug(f"Opened file: {file_path}")
+            return jsonify({"message": "File opened successfully!"}), 200
+        else:
+            logging.error("File not found")
+            return jsonify({"error": "File not found"}), 404
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=55000)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Open Local File with Streamlit (Client)
+Please use with a [streamlit server](https://github.com/ikeshun15/open_local_path_with_streamlit_server).
+
+## Create environment and executable file (Miniconda)
+```bash
+conda create --name python311_open_local_path_with_streamlit_server python=3.11
+```
+
+```bash
+conda activate python311_open_local_path_with_streamlit_client
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+pyinstaller --onefile client_server.py
+```
